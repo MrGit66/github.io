ca781407d562fdd1b520f87c74fe644f9a6aa034
MrGit66 <34916729+MrGit66@users.noreply.github.com>
2025-05-29 16:16:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Grazie per aver compilato il questionario!');
+    // Qui puoi aggiungere la logica per inviare i dati del form a un server
+});
